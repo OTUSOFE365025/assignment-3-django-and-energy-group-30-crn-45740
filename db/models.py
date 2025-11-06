eafec8ac2b52ab6b4c41bc889f765edf8611aad6
nat-ten <nathan.tenn2002@gmail.com>
2025-11-05 19:39:30 -0500

    Update models.py

--- db/models.py
+++ db/models.py
@@ -6,11 +6,9 @@ except Exception:
     print('Exception: Django Not Found, please install it with "pip install django".')
     sys.exit()
 
-
-# Sample User model
-class User(models.Model):
-    name = models.CharField(max_length=50, default="Dan")
-
+class Product(models.Model):
+    name = models.CharField(max_length = 50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    upc_code = models.IntegerField(unique=True)
     def __str__(self):
-        return self.name
-
+        return self.product, self.price, self.upc_code
