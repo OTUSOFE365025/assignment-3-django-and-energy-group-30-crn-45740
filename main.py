ccd3de75c774c6b2ba480e1c4cbc9650d05d3392
nat-ten <nathan.tenn2002@gmail.com>
2025-11-05 19:36:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,30 @@ from db.models import *
 ## START OF APPLICATION
 ############################################################################
 """ Replace the code below with your own """
+Product.objects.create(name='Banana', price=0.59, upc_code=4011)
+Product.objects.create(name='Apple', price=0.99, upc_code=3099)
+Product.objects.create(name='Carrot', price=1.19, upc_code=3011)
+Product.objects.create(name='Onion', price=0.49, upc_code=2034)
 
-# Seed a few users in the database
-User.objects.create(name='Dan')
-User.objects.create(name='Robert')
+quit = False
+while quit == False:
+    print("Enter a UPC code (or type Q to quit): ")
+    code = input()
+    if (code == "Q"):
+        quit = True
+    else:
+        for p in Product.objects.all(): #loop through all models in database
+            try: #if the code is not a number or Q an error occurs when trying to cast code as an int
+                if (int(code) == p.upc_code): #check if code is equal to each products upc code
+                    code_found = True
+                    break
+                else:
+                    code_found = False
+            except:
+                print("Please enter either an integer or Q")
+                break
 
-for u in User.objects.all():
-    print(f'ID: {u.id} \tUsername: {u.name}')
+    if (code_found):
+        print(f'Product: {p.name} \tPrice: {p.price} \tUPC code: {p.upc_code}') #if so, print product
+    else:
+        print("Code not found")
